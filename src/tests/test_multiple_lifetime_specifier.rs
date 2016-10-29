35c1491ee1891ea420f5883f8f1f68a5fe3063b3
towry <tovvry@gmail.com>
2016-10-29 12:23:05 +0800

    Up

--- src/tests/test_multiple_lifetime_specifier.rs
+++ src/tests/test_multiple_lifetime_specifier.rs
@@ -23,19 +23,21 @@ pub fn test_multiple_lifetime_specifier() {
     let var_a = {
         let inner_number1 = 2;
         let local_a = A{a: &outer_number, b: &inner_number1};
+        // this is a return
         local_a.a
     };
 
-    println!("Ok: {}", var_a);
-
+    // println!("Ok: {}", var_a);
 
     let outer_number2 = 3;
     // error
     let var_b = {
         let inner_number2 = 3;
-        let local_b = B{a: &outer_number2, b: &inner_number2};
+        // below will cause an error.
+        // let local_b = B{a: &outer_number2, b: &inner_number2};
+        let local_b = B{a: &outer_number2, b: &outer_number2};
         local_b.a
         // println!("test in the inner: {}", local_b.a);
     };
-    println!("this will not print out: {}", var_b);
+    // println!("this will not print out: {}", var_b);
 }
