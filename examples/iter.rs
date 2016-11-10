88f3b3cca2951c8e05a4c0652da02577dfd6ff05
towry <tovvry@gmail.com>
2016-11-10 12:05:06 +0800

    Add error test

---
+++ examples/iter.rs
@@ -0,0 +1,7 @@
+
+fn main() {
+    let a: &str = "Hello World";
+    for i in a.chars() {
+        print!("{}", i);
+    }
+}
--- examples/test_ref.rs
+++ examples/test_ref.rs
@@ -1,4 +1,6 @@
 
+use std::fs;
+
 fn test<'a>() -> &'a str {
     println!("{}", &*format!("v{}", "hello"));
     "hello"
@@ -6,4 +8,14 @@ fn test<'a>() -> &'a str {
 
 fn main() {
     println!("{}", test());
+
+    let a = fs::File::open("abcde.txt");
+    match a {
+        Ok(_) => {
+            println!("Ok");
+        },
+        Err(_) => {
+            println!("err");
+        }
+    }
 }
