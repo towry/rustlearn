96a69207ac5d0a708841535a3ca70acff7e02dd6
towry <tovvry@gmail.com>
2018-08-29 13:57:11 +0800

    Add macros example

--- examples/macros.rs
+++ examples/macros.rs
@@ -1,7 +1,14 @@
 
-#[macro_use(macros_foo)]
+#[macro_use(macros_foo, map)]
 extern crate rustlearn;
 
 fn main() {
     macros_foo!(x + 3);
+
+    let user = map!(
+        "name" => "Finn",
+        "gender" => "Boy"
+    );
+
+    println!("User {:?}", user);
 }
--- src/macros/mod.rs
+++ src/macros/mod.rs
@@ -3,3 +3,13 @@
 macro_rules! macros_foo {
     (x + $e:expr) => (println!("mode x: {}", $e));
 }
+
+
+#[macro_export]
+macro_rules! map {
+    ( $( $key:expr => $value:expr ),* ) => ({
+        let mut hm = ::std::collections::HashMap::new();
+        $( hm.insert($key, $value); )*
+        hm
+    })
+}
