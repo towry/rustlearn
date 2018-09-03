d199cfcf394a60d1957955e382af1c9b8950cf28
towry <tovvry@gmail.com>
2018-09-03 09:23:11 +0800

    Update comment

--- src/tests/test_move_back.rs
+++ src/tests/test_move_back.rs
@@ -1,9 +1,12 @@
+// Note, below tests is totally false.
 
 #[test]
 fn test_move_back() {
     let a = 3i32;
     let b = a;
     // can not use a here
+    // Update on 1/9.2018: we can use a later, because a implemented the Copy
+    // trait.
 
     // if we do not use let
     // it will alert the immutable error
@@ -18,6 +21,7 @@ fn test_move_back() {
 fn test_move_back_with_mut() {
     let mut a = 3i32;
     let b = a;
+
     a = b;
     assert_eq!(a, 3i32);
     // so we can move it back!
